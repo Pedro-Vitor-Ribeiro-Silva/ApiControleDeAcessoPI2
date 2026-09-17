540a65370b65066c4123f1953785cd5329724405
Pedro-Vitor-Ribeiro-Silva <pedrovitorrbeirosilva@gmail.com>
2026-09-16 21:24:25 -0300

    feat: implementa LogService
    Serviço para listar todos os logs, buscar log por id e buscar os logs de uma pessoa.

---
+++ src/main/java/com/example/controledeacesso/service/LogService.java
@@ -0,0 +1,31 @@
+package com.example.controledeacesso.service;
+
+import com.example.controledeacesso.entity.Log;
+import com.example.controledeacesso.repositories.LogRepository;
+import com.example.controledeacesso.repositories.PessoaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LogService {
+
+    private final PessoaRepository pessoaRepository;
+    private final LogRepository logRepository;
+
+    public List<Log> listarLogs() {
+        return logRepository.findAll();
+    }
+
+    public Log buscarLogPorId(Long id) {
+        return logRepository.findById(id).orElse(null);
+    }
+
+    public List<Log> buscarLogsDeUmaPessoaPorId(String id){
+        return logRepository.findByPessoaId(id);
+    }
+
+
+}
