b637fc6126d08d2d456e4c25c258c25811743c27
Pedro-Vitor-Ribeiro-Silva <pedrovitorrbeirosilva@gmail.com>
2026-09-18 21:58:41 -0300

    feat: adiciona endpoint GET /listar

--- src/main/java/com/example/controledeacesso/controller/PessoaController.java
+++ src/main/java/com/example/controledeacesso/controller/PessoaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -16,6 +17,11 @@ import java.util.Base64;
 public class PessoaController {
 
     private final PessoaService pessoaService;
+
+    @GetMapping("/listar")
+    public List<Pessoa> listarPessoas() {
+        return pessoaService.listarPessoas();
+    }
 
     @PostMapping("/cadastro")
     public ResponseEntity<String> cadastroPessoa(
